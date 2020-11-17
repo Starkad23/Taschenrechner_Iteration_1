40b23c12109b985012f12c9f23f4561b29d79d78
Kai Rusche <mail@kai-rusche.de>
2020-11-17 22:14:27 +0100

    Initialer Comit

---
+++ Taschenrechner_Iteration_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Taschenrechner_Iteration_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte gib den ersten Summanden ein: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte gib den zweiten Summanden ein: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Wandel Text in Ganzahl um
+
+            int ersterSummandAlsZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandAlsZahl = Convert.ToInt32(zweiterSummand);
+
+            int ergebnis = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+            Console.WriteLine("Die Summe ist: " + ergebnis);
+            Console.ReadLine();
+        }
+    }
+}
